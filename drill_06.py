5e58b172a8e8d73e1967b4a142265dafdafdcd15
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-01 15:01:10 +0900

    직선 이동 테스트

--- drill_06.py
+++ drill_06.py
@@ -12,6 +12,29 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 playing = False
     pass
+def move_line():
+    # 두개의 점만 그리기
+    global boy_x, boy_y, arrow_x, arrow_y, frame
+    x1, y1 = boy_x, boy_y
+    x2, y2 = arrow_x, arrow_y
+    for i in range(0, 100):
+        t = i / 100
+
+        boy_x = (1-t)*x1 + t*x2
+        boy_y = (1-t)*y1 + t*y2
+
+        # 버퍼 비우기
+        clear_canvas()
+        # 버퍼 채우기 : 배경, 화살표, 캐릭터 순서
+        ground.draw(400, 300, 800, 600)
+        arrow.clip_draw(0, 0, 50, 50, arrow_x, arrow_y)
+        character.clip_draw(frame * 100, 100, 100, 100, boy_x, boy_y)
+        # character.clip_composite_draw(frame * 38, 80, 38, 80, 0, 'h', x, y, 38, 80)
+        frame = (frame + 1) % 8
+        # 버퍼에 따라 그리기 및 이벤트 입력
+        update_canvas()
+        handle_events()  # 종료 확인용
+        delay(0.05)
 
 open_canvas()
 ground = load_image('TUK_GROUND.png')
@@ -23,27 +46,24 @@ isLeft = False
 
 boy_x = 800 // 2
 boy_y = 600 // 2
-
 arrow_x = random.randrange(0, 800)
 arrow_y = random.randrange(0, 600)
-
 dx = 0
 dy = 0
-
 frame = 0
 
-# fill here
+move_line()
+
 while playing:
 
         # 버퍼 비우기
         clear_canvas()
-        # 버퍼 채우기 : 배경
+        # 버퍼 채우기 : 배경, 화살표, 캐릭터 순서
         ground.draw(400, 300, 800, 600)
-        # 버퍼 채우기 : 캐릭터
-        character.clip_draw(frame * 100, 100, 100, 100, boy_x, boy_y)
         arrow.clip_draw(0, 0, 50, 50, arrow_x, arrow_y)
-        frame = (frame + 1) % 8
+        character.clip_draw(frame * 100, 100, 100, 100, boy_x, boy_y)
         # character.clip_composite_draw(frame * 38, 80, 38, 80, 0, 'h', x, y, 38, 80)
+        frame = (frame + 1) % 8
 
         # 좌표 이동
 
