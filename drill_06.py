40e48678ed8a461efcb2dcec2db56d81689a2116
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-10-01 15:18:09 +0900

    이동 중에도 종료가 될 수 있게함

--- drill_06.py
+++ drill_06.py
@@ -2,6 +2,17 @@
 from pico2d import *
 import random
 
+
+def handle_events():
+    global playing
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            playing = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                playing = False
+    pass
 def buffer_draw():
     global frame
     # 버퍼 비우기
@@ -15,31 +26,22 @@ def buffer_draw():
     else :
         character.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', boy_x, boy_y, 100, 100)
     frame = (frame + 1) % 8
-    # 버퍼에 따라 그리기 및 이벤트 입력
+    # 버퍼에 따라 그리기
     update_canvas()
-    handle_events()  # 종료 확인용
-    pass
-def handle_events():
-    global playing
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            playing = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                playing = False
-    pass
 def move_line():
     # 두개의 점만 그리기
     global boy_x, boy_y, arrow_x, arrow_y, frame
     x1, y1 = boy_x, boy_y
     x2, y2 = arrow_x, arrow_y
+
     for i in range(0, 100):
         t = i / 100
 
         boy_x = (1-t)*x1 + t*x2
         boy_y = (1-t)*y1 + t*y2
         buffer_draw()
+        handle_events()  # 종료 확인용
+        if playing == False: break
         delay(0.05)
 
 open_canvas()
@@ -52,18 +54,14 @@ isLeft = False
 
 boy_x = 800 // 2
 boy_y = 600 // 2
-#arrow_x = random.randrange(0, 800)
-#arrow_y = random.randrange(0, 600)
-arrow_x = 0
-arrow_y = 600   # 좌상단 테스트 (반전이동)
-dx = 0
-dy = 0
+arrow_x = random.randrange(0, 800)
+arrow_y = random.randrange(0, 600)
 frame = 0
 
 move_line()
-
 while playing:
         buffer_draw()
+        handle_events()  # 종료 확인용
         delay(0.05)
 
 close_canvas()
